5e2e96feb70c28e50b13fd4ce9997a4f65ec31bc
leonardo534 <leonardoredes534@gmail.com>
2020-03-21 13:21:44 -0300

    Adicionado TextView onde mostra o resultado

--- app/src/main/java/com/leonardosilva/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/leonardosilva/pedrapapeloutesoura/MainActivity.java
@@ -5,16 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imagemResultado = findViewById(R.id.imageResultado);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void selecionadoPedra(View view) {
@@ -29,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
         this.opcaoSelecionada("tesoura");
     }
 
-    public void opcaoSelecionada(String opcao){
+    public void opcaoSelecionada(String opcaoUsuario){
+        ImageView imagemResultado = findViewById(R.id.imageResultado);
+        TextView resultado = findViewById(R.id.textView3);
         int numero = new Random().nextInt(3);
 
         String opcaoes[] = {"pedra", "papel", "tesoura"};
@@ -45,6 +49,20 @@ public class MainActivity extends AppCompatActivity {
             case "tesoura" :
                 imagemResultado.setImageResource(R.drawable.tesoura);
                 break;
+        }
+
+        if((opcaoApp=="tesoura" && opcaoUsuario=="papel") ||
+                (opcaoApp=="pedra" && opcaoUsuario=="tesoura") ||
+                (opcaoApp=="papel" && opcaoUsuario=="pedra")){ //App ganha
+            resultado.setText("Você perdeu");
+
+        }else if((opcaoApp=="papel" && opcaoUsuario=="tesoura") ||
+                (opcaoApp=="tesoura" && opcaoUsuario=="pedra") ||
+                (opcaoApp=="pedra" && opcaoUsuario=="papel")){ //Usuario ganha
+            resultado.setText("Você ganhou");
+
+        }else{
+            resultado.setText("Empatamos");
         }
 
     }
